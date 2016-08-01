a6a85d1d4950ebd357e42d65a61f71f96bfe7f71
Frank Milburn <fmilburn3@users.noreply.github.com>
2016-07-31 22:57:32 -0700

    Update Utilities.ino

--- Utilities.ino
+++ Utilities.ino
@@ -1,20 +1,43 @@
-void blinkForever(int delayTime){
+// -+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
+void statusLED(int toggleTime, long delayTime){
 /*
- toggles the status LED forever at a regular interval equal to delayTime
-*/
-  for(;;){
-    digitalWrite(STATUS_LED, HIGH);
-    delay(delayTime);
-    digitalWrite(STATUS_LED, LOW);
-    delay(delayTime);
+ * toggleTime is the interval (ms) between toggles
+ * delayTime delays everything for a specified length of time where
+ *    -x  (any negative number) delays forever - i.e. endless blinking loop
+ *     0  toggles the LED from present state and immediately returns
+ *        (toggleTime can be any value)
+ *    +x  delays for x milliseconds and then returns
+ *
+ * Note: delayTime must be positive and toggleTime should be less than delayTime
+ */
+  static int toggle = 0;
+  if (delayTime == 0){                    // toggle once
+    toggle = !toggle;
+    digitalWrite(STATUS_LED, toggle);
+  }
+  else if (delayTime > 0) {               // toggle for a fixed amount of time
+    unsigned long stopTime = millis() + delayTime;
+    while (millis() < stopTime){
+      toggle = !toggle;
+      digitalWrite(STATUS_LED, toggle);
+      delay(toggleTime);
+    }
+  }
+  else {                                  // negative number - toggle forever
+    for(;;){
+      toggle = !toggle;
+      digitalWrite(STATUS_LED, toggle);
+      delay(toggleTime);
+    }
   }
 }
 // -+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
 void getLine(char array[], int length){
 /*
- reads incoming serial characters into array[] until carriage return or line
- feed is recognized or no new characters are available
-*/
+ * reads incoming serial characters into array[] until carriage return or line
+ * feed is recognized or no new characters are available.  length is the
+ * maximum length of the array.
+ */
   int i = 0;
   int keepLooping = 1;
   while (keepLooping){
@@ -42,3 +65,4 @@ void getLine(char array[], int length){
     }
   }
 }
+
