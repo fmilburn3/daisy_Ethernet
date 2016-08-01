670aab777f01aa495dede8e8a853ec512306f02a
Frank Milburn <fmilburn3@users.noreply.github.com>
2016-07-31 23:04:03 -0700

    Update getServerPort.ino

--- getServerPort.ino
+++ getServerPort.ino
@@ -1,51 +1,46 @@
 // -+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
-void getServerPort(){
-  Serial.begin(38400);
-  Serial.println("\ngetServerPort...");
-  if (Ethernet.begin(mac) == 0) {
-    Serial.println("\nFailed to configure Ethernet...");
-    while(true);                            // No point in carrying on          // ??????? status LED not set yet
+int getServerPort(){
+/*
+ * Gets a server and port from marifact and returns the following information:
+ *  0 = marifact did not return "200 OK"
+ *  1 = All OK
+ */
+  int returnCode = 1;
+  if (DEBUG > 0){
+    Serial.println("\ngetServerPort...");
   }
-
-  delay(1000);                              // Let Ethernet stabilize
-   
-  if (DEBUG){
-    Serial.println("Configured Ethernet, attempt to connect...");
-  }
-  if (client.connect("api.marifact.net", 80)) {
-    char clientStr1[] = "GET /ais_sources/initialize/";
-    char clientStr2[] = " HTTP/1.1";
-    strcpy(nmea, clientStr1);
-    strcat(nmea, ID);
-    strcat(nmea, clientStr2);
-    Serial.println(nmea); 
-    // -------------------------------------------------------------------- abuse of nmea ?????????????????????
-    client.println(nmea);
-    client.println("Host: api.marifact.net");    
-    client.println("");
-
-    Serial.println("Connected and made a HTTP request...");                                                         // <----
-  } 
-  else {
-    Serial.println("connection failed");
-  }
+  int numLoops = 0;
   while(serverPort == 0){
-    // Look for 4 zero length strings in a row
-    // ---------------------------------------------------------- check for 200 OK
+    numLoops++;
     static int zeroLength = 0;
     if (client.available() && (zeroLength < 4)) {
       getLine(nmea, MAXLEN);
+      if (numLoops == 0){
+        // first pass, check to see if "200 OK" is in the string
+        char *ptr200;
+        ptr200 = strstr(nmea,clientStr4);
+        if (ptr200 == NULL){          // did server return "200 OK"?
+          if (DEBUG > 1){
+            Serial.println("Server did not return 200 OK");
+          }
+          returnCode = 0;
+        } 
+      }
       if (strlen(nmea) > 0){
         if (zeroLength < 4) {
           zeroLength = 0;
         }
-        //Serial.print("New line: ");
-        //Serial.println(nmea);
+        if (DEBUG > 2){
+          Serial.print("New line: ");
+          Serial.println(nmea);
+        }
       }
       else {
         zeroLength++;
-        //Serial.print("===>  zero length: ");
-        //Serial.println(zeroLength);
+        if (DEBUG > 2){
+          Serial.print("===>  zero length: ");
+          Serial.println(zeroLength);
+        }
       }
     }
     if (zeroLength == 4) {
@@ -68,23 +63,23 @@ void getServerPort(){
         Serial.println(serverPort);
       }
       zeroLength = 0;
-    }
-/*                                                              Should I have removed this ???????  
+    }                                                          
     // if the server's disconnected, stop the client:
     if (!client.connected()) {
       Serial.println("\nDisconnecting...");
       client.stop();
     }
-*/
   }
+  return returnCode;
 }
 
 // -+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
 void convertServerString(char a[], byte num[]) {
-  // places 4 numbers from the period separated string (a) into a byte
-  // array (num).  For example, if a[] = "123.222.78.9" the byte array
-  // becomes num[0] = 123, num[1] = 222, num[2] = 78, num[3] = 9
-  
+/*
+ * places 4 numbers from the period separated string (a) into a byte
+ * array (num).  For example, if a[] = "123.222.78.9" the byte array
+ * becomes num[0] = 123, num[1] = 222, num[2] = 78, num[3] = 9
+ */ 
   num[0] = (byte)(atoi(a));               // 1st number - num[0]
   char *ptr1, *ptr2, *ptr3;               // pointers to the '.' in string a
   ptr1 = strchr(a, '.');
@@ -102,3 +97,4 @@ void convertServerString(char a[], byte num[]) {
   ptr3 = strrchr(a, '.');
   num[3] = (byte)(atoi(ptr3+1));          // 4th number - num[3]
 }
+
