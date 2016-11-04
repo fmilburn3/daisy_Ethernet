724c6a06ad6187ae05d47d111eef4616e5fd2ca9
Frank Milburn <fmilburn3@users.noreply.github.com>
2016-11-03 20:49:57 -0700

    Create initUDP.ino

---
+++ source/daisy_ethernet_v_3/initUDP.ino
@@ -0,0 +1,11 @@
+// -+----1----+----2----+----3----+----4----+----5----+----6----+----7----+----8
+void initUDP(){
+/*
+ * Initiates UDP
+ */
+  udp.begin(8888);
+  delay(1000);                                        // let Ethernet stabilize
+  if (DEBUG > 0){
+    Serial.println("initialized UDP... ready to send data"); 
+  }
+}
